11a2ad1b682941581bac545614b827826e4cf6bd
vipincodewave <145666698+vipincodewave@users.noreply.github.com>
2024-07-28 21:06:54 +0530

    vipin

--- index.js
+++ index.js
@@ -8,55 +8,6 @@ app.get("/", (req, res, next) => {
   });
 });
 
-app.get("/path", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from path!",
-  });
-});
-
-app.get("/pathss", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from paths!",
-    statusCode: 200,
-    statusCodeDescription: "OK"
-  })
-});
-
-app.get("/hello-world", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from hello!",
-  });
-});
-
-app.post("/hello-world/:id", (req, res, next) => {
-  const requestBody = req.body
-  return res.status(200).json({
-    message: "Hello from hello!",
-    data:requestBody
-  });
-});
-
-app.post("/hell/:id", (req, res, next) => {
-  const requestBody = req.body
-  return res.status(200).json({
-    message: "Hello from hi!",
-    data:requestBody
-  });
-});
-
-app.get("/custom-error", (req, res, next) => {
-  throw new Error("This is a custom error");
-});
-
-app.get('/custom-error/:id', (req, res, next) => {
-  const id = parseInt(req.params.id);
-  if (Number.isNaN(id)) {
-    throw new Error("Invalid id");
-  }
-  return res.status(200).json({
-    message: `Hello from custom error with id: ${id}`,
-  });
-})
 
 app.use((req, res, next) => {
   return res.status(404).json({
